3e65d350ea8207de14f3671aabaecd58198eb268
Daikone <eduardoarzolay@gmail.com>
2024-02-07 21:36:49 -0400

    refactor: extend tailwind colors theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'ucab-green': '#007934',
+        'ucab-blue': '#41B4E5',
+        'ucab-yellow': '#FFD200',
+        'brand-white': '#f9f9f9'
+      }
+    }
   },
   plugins: []
 }

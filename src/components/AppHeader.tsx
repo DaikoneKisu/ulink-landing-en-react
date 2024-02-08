45e45d0c69b05c23f2d9938ae6d960b3cd4d190d
Daikone <eduardoarzolay@gmail.com>
2024-02-07 21:39:29 -0400

    feat: add AppHeader

---
+++ src/components/AppHeader.tsx
@@ -0,0 +1,17 @@
+import headerImg from '$assets/app-header-img.svg'
+
+export function AppHeader() {
+  return (
+    <header className="flex">
+      <a href="/" className="h-[65px] w-[167px] flex-shrink-0 flex-grow-0">
+        <img className="h-[65px] w-[167px]" src={headerImg} alt="ULink full logo" />
+      </a>
+      <nav className="flex w-full items-center justify-around [&>a]:text-lg [&>a]:font-bold [&>a]:capitalize">
+        <a href="#benefits">Beneficios</a>
+        <a href="#3">Objetivos</a>
+        <a href="#4">Catálogo</a>
+        <a href="#5">Iniciar sesión</a>
+      </nav>
+    </header>
+  )
+}
